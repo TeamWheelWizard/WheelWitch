bff5f14473baca39201e182060bb87330bcc957b
jamie <git@skilet.ro>
2026-09-08 23:39:00 +0100

    chore(deps): drop com.google.android.material

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -118,7 +118,6 @@ dependencies {
   implementation(libs.androidx.lifecycle.runtime.ktx)
   implementation(libs.androidx.lifecycle.runtime.compose)
   implementation(libs.androidx.lifecycle.viewmodel.compose)
-  implementation(libs.material)
   implementation(libs.okhttp)
   implementation(libs.rollingnumbers)
   implementation(libs.timber)
